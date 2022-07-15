a7d96dd847b815a3064eff85ff0c2c736f37ef1f
caiosev <kaiosev9@gmail.com>
2022-07-15 10:37:55 -0300

    prova voo

--- src/components/modals/Voo/index.jsx
+++ src/components/modals/Voo/index.jsx
@@ -1,31 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
+import { Form } from '@unform/web';
 import * as S from './styled';
+import Radio from '../../Form/radio';
 
 export default function Voo({ setValor, setIdProf, close }) {
     const [pontos, setPontos] = useState(undefined);
-    const [q1, setQ1] = useState(undefined);
-    const [q2, setQ2] = useState(undefined);
-    const [q3, setQ3] = useState(undefined);
-    const [q4, setQ4] = useState(undefined);
-    const [q5, setQ5] = useState(undefined);
     const [nota, setNota] = useState(undefined);
     const res = ['1', '2', '3', '1', '4'];
 
-    const handleSubmit = () => {
-        if (
-            q1 === undefined ||
-            q2 === undefined ||
-            q3 === undefined ||
-            q4 === undefined ||
-            q5 === undefined
-        ) {
+    const handleSubmit = (data) => {
+        const resAluno = Object.values(data);
+        if (resAluno.length < 5) {
             toast.error('Responda todas as questoes');
             return;
         }
         let soma = 0;
-        [q1, q2, q3, q4, q5].forEach((element, i) => {
+        resAluno.forEach((element, i) => {
             if (element === res[i]) {
                 soma += 1;
             }
@@ -59,153 +51,161 @@ export default function Voo({ setValor, setIdProf, close }) {
             default:
                 break;
         }
-        setValor(pontos);
         close();
+        setValor(pontos);
+        toast.success(
+            'Prova realizada com Sucesso, acesse a aba de provas enviadas'
+        );
     }, [pontos]);
+
+    const optionsQ1 = [
+        {
+            id: 1,
+            value: '1',
+            label: 'Suba',
+        },
+        {
+            id: 2,
+            value: '2',
+            label: 'Venha',
+        },
+        {
+            id: 3,
+            value: '3',
+            label: 'Levite',
+        },
+        {
+            id: 4,
+            value: '4',
+            label: 'Pra cima',
+        },
+    ];
+    const optionsQ2 = [
+        {
+            id: 5,
+            value: '1',
+            label: 'Alvo dumbledore',
+        },
+        {
+            id: 6,
+            value: '2',
+            label: 'Elliot Smethwyck',
+        },
+        {
+            id: 7,
+            value: '3',
+            label: 'Bob Ollerton',
+        },
+        {
+            id: 8,
+            value: '4',
+            label: 'Rolanda Hooch',
+        },
+    ];
+
+    const optionsQ3 = [
+        {
+            id: 9,
+            value: '1',
+            label: 'Tom e Newton',
+        },
+        {
+            id: 10,
+            value: '2',
+            label: 'Gelbert e Credence',
+        },
+        {
+            id: 11,
+            value: '3',
+            label: 'Os irmaos Ollerton',
+        },
+        {
+            id: 12,
+            value: '4',
+            label: 'Gwendolyn Morgan',
+        },
+    ];
+
+    const optionsQ4 = [
+        {
+            id: 13,
+            value: '1',
+            label: 'Pomorim',
+        },
+        {
+            id: 14,
+            value: '2',
+            label: 'Podrorim',
+        },
+        {
+            id: 15,
+            value: '3',
+            label: 'Prolorim',
+        },
+        {
+            id: 16,
+            value: '4',
+            label: 'Promirim',
+        },
+    ];
+
+    const optionsQ5 = [
+        {
+            id: 17,
+            value: '1',
+            label: 'X',
+        },
+        {
+            id: 18,
+            value: '2',
+            label: 'II',
+        },
+        {
+            id: 19,
+            value: '3',
+            label: 'IX',
+        },
+        {
+            id: 20,
+            value: '4',
+            label: 'XI',
+        },
+    ];
 
     return (
         <S.Container>
-            <h1>Prova de Voo</h1>
+            <h1>Prova de Transfiguração</h1>
             <h2>Questões</h2>
-            <form action="">
-                <label
-                    htmlFor="q1"
-                    value={q1}
-                    onChange={(e) => setQ1(e.target.value)}
-                >
-                    {' '}
-                    1 - Qual a palavra que deve ser dita para a vassoura ir para
-                    suas mãos?
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q1" id="" value="1" />
-                            Suba
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q1" id="" value="2" />
-                            Venha
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q1" id="" value="3" />
-                            Levite
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q1" id="" value="4" />
-                            Pra Cima
-                        </div>
-                    </div>
-                </label>
-                <label
-                    htmlFor="q2"
-                    value={q2}
-                    onChange={(e) => setQ2(e.target.value)}
-                >
-                    {' '}
-                    2 - Quem inventou o amuleto de amortecimento?
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q2" id="" value="1" />{' '}
-                            Alvo dumbledore
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q2" id="" value="2" />
-                            Elliot Smethwyck
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q2" id="" value="3" />
-                            Bob Ollerton
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q2" id="" value="4" />
-                            Rolanda Hooch
-                        </div>
-                    </div>
-                </label>
-                <label
-                    htmlFor="q3"
-                    value={q3}
-                    onChange={(e) => setQ3(e.target.value)}
-                >
-                    {' '}
-                    3 - Quem foram os fundadores da Cleansweep Broom Company?
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q3" id="" value="1" />
-                            Tom e Newton
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q3" id="" value="2" />
-                            Gelbert e Credence
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q3" id="" value="3" />
-                            Os irmaos Ollerton
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q3" id="" value="4" />
-                            Gwendolyn Morgan
-                        </div>
-                    </div>
-                </label>
-                <label
-                    htmlFor="q4"
-                    value={q4}
-                    onChange={(e) => setQ4(e.target.value)}
-                >
-                    {' '}
-                    4 - O pomo de ouro é baseado em uma criatura, qual é seu
-                    nome?
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q4" id="" value="1" />
-                            Pomorim
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q4" id="" value="2" />
-                            Podrorim
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q4" id="" value="3" />
-                            Prolorim
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q4" id="" value="4" />
-                            Promirim
-                        </div>
-                    </div>
-                </label>
-                <label
-                    htmlFor="q5"
-                    value={q5}
-                    onChange={(e) => setQ5(e.target.value)}
-                >
-                    {' '}
-                    5 - Em que seculo surgiu o Quadribol?
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q5" id="" value="1" />X
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q5" id="" value="2" />A II
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q5" id="" value="3" />
-                            IX
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q5" id="" value="4" />
-                            XI
-                        </div>
-                    </div>
-                </label>
-                <button
-                    type="button"
-                    onClick={handleSubmit}
-                    disabled={nota !== undefined}
-                >
-                    Enviar
-                </button>
-            </form>
+            <Form onSubmit={handleSubmit}>
+                <Radio
+                    name="q1"
+                    label="1 - Qual a palavra que deve ser dita para a vassoura ir para suas mãos?"
+                    options={optionsQ1}
+                />
+                <Radio
+                    name="q2"
+                    label="2 - Quantas letras existem no alfabeto de transfiguração?"
+                    options={optionsQ2}
+                />
+                <Radio
+                    name="q3"
+                    label="3 - Quem foram os fundadores da Cleansweep Broom Company?"
+                    options={optionsQ3}
+                />
+                <Radio
+                    name="q4"
+                    label="4 - O pomo de ouro é baseado em uma criatura, qual é seu nome?"
+                    options={optionsQ4}
+                />
+                <Radio
+                    name="q5"
+                    label="5 - Em que seculo surgiu o Quadribol?"
+                    options={optionsQ5}
+                />
+                {nota}
+                {pontos && '/5'}
+                <button type="submit">Enviar</button>
+            </Form>
         </S.Container>
     );
 }
