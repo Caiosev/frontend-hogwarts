7b641b4fb0a769dc9cc75cbd70f8fe695e241146
caiosev <kaiosev9@gmail.com>
2022-07-15 10:25:23 -0300

    prova transfiguracao

--- src/components/modals/Trans/index.jsx
+++ src/components/modals/Trans/index.jsx
@@ -1,31 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
+import { Form } from '@unform/web';
 import * as S from './styled';
+import Radio from '../../Form/radio';
 
 export default function Trans({ setValor, setIdProf, close }) {
     const [pontos, setPontos] = useState(undefined);
-    const [q1, setQ1] = useState(undefined);
-    const [q2, setQ2] = useState(undefined);
-    const [q3, setQ3] = useState(undefined);
-    const [q4, setQ4] = useState(undefined);
-    const [q5, setQ5] = useState(undefined);
     const [nota, setNota] = useState(undefined);
     const res = ['1', '3', '1', '3', '4'];
 
-    const handleSubmit = () => {
-        if (
-            q1 === undefined ||
-            q2 === undefined ||
-            q3 === undefined ||
-            q4 === undefined ||
-            q5 === undefined
-        ) {
+    const handleSubmit = (data) => {
+        const resAluno = Object.values(data);
+        if (resAluno.length < 5) {
             toast.error('Responda todas as questoes');
             return;
         }
         let soma = 0;
-        [q1, q2, q3, q4, q5].forEach((element, i) => {
+        resAluno.forEach((element, i) => {
             if (element === res[i]) {
                 soma += 1;
             }
@@ -59,156 +51,161 @@ export default function Trans({ setValor, setIdProf, close }) {
             default:
                 break;
         }
-        setValor(pontos);
         close();
+        setValor(pontos);
+        toast.success(
+            'Prova realizada com Sucesso, acesse a aba de provas enviadas'
+        );
     }, [pontos]);
+
+    const optionsQ1 = [
+        {
+            id: 1,
+            value: '1',
+            label: 'Lei de Gamp',
+        },
+        {
+            id: 2,
+            value: '2',
+            label: 'Lei de Mutabilidade',
+        },
+        {
+            id: 3,
+            value: '3',
+            label: 'Lei de Gramp',
+        },
+        {
+            id: 4,
+            value: '4',
+            label: 'Lei de Imutabilidade',
+        },
+    ];
+    const optionsQ2 = [
+        {
+            id: 5,
+            value: '1',
+            label: '20',
+        },
+        {
+            id: 6,
+            value: '2',
+            label: '24',
+        },
+        {
+            id: 7,
+            value: '3',
+            label: '26',
+        },
+        {
+            id: 8,
+            value: '4',
+            label: '28',
+        },
+    ];
+
+    const optionsQ3 = [
+        {
+            id: 9,
+            value: '1',
+            label: 'Branco',
+        },
+        {
+            id: 10,
+            value: '2',
+            label: 'Azul',
+        },
+        {
+            id: 11,
+            value: '3',
+            label: 'Verde',
+        },
+        {
+            id: 12,
+            value: '4',
+            label: 'Preta',
+        },
+    ];
+
+    const optionsQ4 = [
+        {
+            id: 13,
+            value: '1',
+            label: 'Balançar a varinha para esquerda duas vezes',
+        },
+        {
+            id: 14,
+            value: '2',
+            label: 'Balançar a varinha para direita duas vezes',
+        },
+        {
+            id: 15,
+            value: '3',
+            label: 'Segurar a varinha para esquerda',
+        },
+        {
+            id: 16,
+            value: '4',
+            label: 'Balançar a varinha para direita',
+        },
+    ];
+
+    const optionsQ5 = [
+        {
+            id: 17,
+            value: '1',
+            label: 'Cadeira',
+        },
+        {
+            id: 18,
+            value: '2',
+            label: 'Cachorro',
+        },
+        {
+            id: 19,
+            value: '3',
+            label: 'Mesa',
+        },
+        {
+            id: 20,
+            value: '4',
+            label: 'Caixa de Fosforos',
+        },
+    ];
 
     return (
         <S.Container>
             <h1>Prova de Transfiguração</h1>
             <h2>Questões</h2>
-            <form action="">
-                <label
-                    htmlFor="q1"
-                    value={q1}
-                    onChange={(e) => setQ1(e.target.value)}
-                >
-                    {' '}
-                    1 - Como é chamada a lei que impoe limite e regula as
-                    Tranfigurações?
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q1" id="" value="1" />
-                            Lei de Gamp
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q1" id="" value="2" />
-                            Lei de Mutabilidade
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q1" id="" value="3" />
-                            Lei de Gramp
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q1" id="" value="4" />
-                            Lei de Imutabilidade
-                        </div>
-                    </div>
-                </label>
-                <label
-                    htmlFor="q2"
-                    value={q2}
-                    onChange={(e) => setQ2(e.target.value)}
-                >
-                    {' '}
-                    2 - Quantas letras existem no alfabeto de transfiguração?
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q2" id="" value="1" />
-                            20
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q2" id="" value="2" />
-                            24
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q2" id="" value="3" />
-                            26
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q2" id="" value="4" />
-                            28
-                        </div>
-                    </div>
-                </label>
-                <label
-                    htmlFor="q3"
-                    value={q3}
-                    onChange={(e) => setQ3(e.target.value)}
-                >
-                    {' '}
-                    3 - Qual a cor da luz deve ser emitida quando a magia de
-                    Troca é executada com perfeição?
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q3" id="" value="1" />
-                            Branco
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q3" id="" value="2" />
-                            Azul
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q3" id="" value="3" />
-                            Verde
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q3" id="" value="4" />
-                            Preta
-                        </div>
-                    </div>
-                </label>
-                <label
-                    htmlFor="q4"
-                    value={q4}
-                    onChange={(e) => setQ4(e.target.value)}
-                >
-                    {' '}
-                    4 - Qual o movimento de mãos é feito para conjurar o feitiço
-                    Flintifors?
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q4" id="" value="1" />
-                            Balançar a varinha para esquerda duas vezes
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q4" id="" value="2" />
-                            Balançar a varinha para direita duas vezes
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q4" id="" value="3" />
-                            Segurar a varinha para esquerda
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q4" id="" value="4" />
-                            Balançar a varinha para direita
-                        </div>
-                    </div>
-                </label>
-                <label
-                    htmlFor="q5"
-                    value={q5}
-                    onChange={(e) => setQ5(e.target.value)}
-                >
-                    {' '}
-                    5 - O feitiço Flintifors transforma o alvo em:
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q5" id="" value="1" />
-                            Cadeira
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q5" id="" value="2" />
-                            Cachorro
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q5" id="" value="3" />
-                            Mesa
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q5" id="" value="4" />
-                            Caixa de Fosforos
-                        </div>
-                    </div>
-                </label>
-                <button
-                    type="button"
-                    onClick={handleSubmit}
-                    disabled={nota !== undefined}
-                >
-                    Enviar
-                </button>
-            </form>
+            <Form onSubmit={handleSubmit}>
+                <Radio
+                    name="q1"
+                    label="1 - Como é chamada a lei que impoe limite e regula as Tranfigurações?"
+                    options={optionsQ1}
+                />
+                <Radio
+                    name="q2"
+                    label="2 - Quantas letras existem no alfabeto de transfiguração?"
+                    options={optionsQ2}
+                />
+                <Radio
+                    name="q3"
+                    label="3 - Qual a cor da luz deve ser emitida quando a magia de Troca é executada com perfeição?"
+                    options={optionsQ3}
+                />
+                <Radio
+                    name="q4"
+                    label="4 - Qual o movimento de mãos é feito para conjurar o feitiço Flintifors?"
+                    options={optionsQ4}
+                />
+                <Radio
+                    name="q5"
+                    label="5 - O feitiço Flintifors transforma o alvo em:"
+                    options={optionsQ5}
+                />
+                {nota}
+                {pontos && '/5'}
+                <button type="submit">Enviar</button>
+            </Form>
         </S.Container>
     );
 }
