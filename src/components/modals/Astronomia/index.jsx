04aa205aaf06fe370ee07b199e3cb48f247a9e4b
caiosev <kaiosev9@gmail.com>
2022-07-15 07:27:06 -0300

    prova astronomia

--- src/components/modals/Astronomia/index.jsx
+++ src/components/modals/Astronomia/index.jsx
@@ -1,31 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
+import { Form } from '@unform/web';
 import * as S from './styled';
+import Radio from '../../Form/radio';
 
 export default function Astronomia({ setValor, setIdProf, close }) {
     const [pontos, setPontos] = useState(undefined);
-    const [q1, setQ1] = useState(undefined);
-    const [q2, setQ2] = useState(undefined);
-    const [q3, setQ3] = useState(undefined);
-    const [q4, setQ4] = useState(undefined);
-    const [q5, setQ5] = useState(undefined);
     const [nota, setNota] = useState(undefined);
     const res = ['96', '5', 'venus', '63', '4'];
 
-    const handleSubmit = () => {
-        if (
-            q1 === undefined ||
-            q2 === undefined ||
-            q3 === undefined ||
-            q4 === undefined ||
-            q5 === undefined
-        ) {
+    const handleSubmit = (data) => {
+        const resAluno = Object.values(data);
+        if (resAluno.length < 5) {
             toast.error('Responda todas as questoes');
             return;
         }
         let soma = 0;
-        [q1, q2, q3, q4, q5].forEach((element, i) => {
+        resAluno.forEach((element, i) => {
             if (element === res[i]) {
                 soma += 1;
             }
@@ -59,155 +51,161 @@ export default function Astronomia({ setValor, setIdProf, close }) {
             default:
                 break;
         }
-        setValor(pontos);
         close();
+        setValor(pontos);
+        toast.success(
+            'Prova realizada com Sucesso, acesse a aba de provas enviadas'
+        );
     }, [pontos]);
+
+    const optionsQ1 = [
+        {
+            id: 1,
+            value: '90',
+            label: '90%',
+        },
+        {
+            id: 2,
+            value: '85',
+            label: '85%',
+        },
+        {
+            id: 3,
+            value: '82',
+            label: '82%',
+        },
+        {
+            id: 4,
+            value: '96',
+            label: '96%',
+        },
+    ];
+    const optionsQ2 = [
+        {
+            id: 5,
+            value: '5',
+            label: '5',
+        },
+        {
+            id: 6,
+            value: '8',
+            label: '8',
+        },
+        {
+            id: 7,
+            value: '1',
+            label: '1',
+        },
+        {
+            id: 8,
+            value: '3',
+            label: '3',
+        },
+    ];
+
+    const optionsQ3 = [
+        {
+            id: 9,
+            value: 'mercurio',
+            label: 'Mercúrio',
+        },
+        {
+            id: 10,
+            value: 'plutao',
+            label: 'Plutão',
+        },
+        {
+            id: 11,
+            value: 'marte',
+            label: 'Marte',
+        },
+        {
+            id: 12,
+            value: 'venus',
+            label: 'Vênus',
+        },
+    ];
+
+    const optionsQ4 = [
+        {
+            id: 13,
+            value: '34',
+            label: '34',
+        },
+        {
+            id: 14,
+            value: '33',
+            label: '33',
+        },
+        {
+            id: 15,
+            value: '63',
+            label: '63',
+        },
+        {
+            id: 16,
+            value: '25',
+            label: '25',
+        },
+    ];
+
+    const optionsQ5 = [
+        {
+            id: 17,
+            value: '5',
+            label: '5',
+        },
+        {
+            id: 18,
+            value: '3',
+            label: '3',
+        },
+        {
+            id: 19,
+            value: '4',
+            label: '4',
+        },
+        {
+            id: 20,
+            value: '6',
+            label: '6',
+        },
+    ];
 
     return (
         <S.Container>
             <h1>Prova de Astronomia</h1>
             <h2>Questões</h2>
-            <form action="">
-                <label
-                    htmlFor="q1"
-                    value={q1}
-                    onChange={(e) => setQ1(e.target.value)}
-                >
-                    {' '}
-                    1 - Energia e matéria escuras representam ___ do universo?
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q1" id="" value="90" />
-                            90%
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q1" id="" value="85" />
-                            85%
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q1" id="" value="82" />
-                            82%
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q1" id="" value="96" />
-                            96%
-                        </div>
-                    </div>
-                </label>
-                <label
-                    htmlFor="q2"
-                    value={q2}
-                    onChange={(e) => setQ2(e.target.value)}
-                >
-                    {' '}
-                    2 - Quantos planetas anões existem em nosso sistema solar?
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q2" id="" value="5" />5
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q2" id="" value="8" />8
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q2" id="" value="1" />1
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q2" id="" value="3" />3
-                        </div>
-                    </div>
-                </label>
-                <label
-                    htmlFor="q3"
-                    value={q3}
-                    onChange={(e) => setQ3(e.target.value)}
-                >
-                    {' '}
-                    3 - Qual o planeta mais quente do sistema solar?
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q3" id="" value="venus" />
-                            Vênus
-                        </div>
-                        <div className="">
-                            <input
-                                type="radio"
-                                name="q3"
-                                id=""
-                                value="mercurio"
-                            />
-                            Mercúrio
-                        </div>
-                        <div className="">
-                            <input
-                                type="radio"
-                                name="q3"
-                                id=""
-                                value="plutao"
-                            />
-                            Plutão
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q3" id="" value="marte" />
-                            Marte
-                        </div>
-                    </div>
-                </label>
-                <label
-                    htmlFor="q4"
-                    value={q4}
-                    onChange={(e) => setQ4(e.target.value)}
-                >
-                    {' '}
-                    4 - Quantas luas o planeta Júpiter possui?
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q4" id="" value="34" />
-                            34
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q4" id="" value="33" />
-                            33
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q4" id="" value="63" />
-                            63
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q4" id="" value="25" />
-                            25
-                        </div>
-                    </div>
-                </label>
-                <label
-                    htmlFor="q5"
-                    value={q5}
-                    onChange={(e) => setQ5(e.target.value)}
-                >
-                    {' '}
-                    5 - Quantos planetas gasosos existem?
-                    <div className="options">
-                        <div className="">
-                            <input type="radio" name="q5" id="" value="5" />5
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q5" id="" value="3" />3
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q5" id="" value="4" />4
-                        </div>
-                        <div className="">
-                            <input type="radio" name="q5" id="" value="6" />6
-                        </div>
-                    </div>
-                </label>
-                <button
-                    type="button"
-                    onClick={handleSubmit}
-                    disabled={nota !== undefined}
-                >
-                    Enviar
-                </button>
-            </form>
+            <Form onSubmit={handleSubmit}>
+                <Radio
+                    name="q1"
+                    label=" 1 - Energia e matéria escuras representam ___ do universo?"
+                    options={optionsQ1}
+                />
+                <Radio
+                    name="q2"
+                    label="2 - Quantos planetas anões existem em nosso sistema solar?"
+                    options={optionsQ2}
+                />
+                <Radio
+                    name="q3"
+                    label="3 - Qual o planeta mais quente do sistema solar?"
+                    options={optionsQ3}
+                />
+                <Radio
+                    name="q4"
+                    label="4 - Quantas luas o planeta Júpiter possui?"
+                    options={optionsQ4}
+                />
+                <Radio
+                    name="q5"
+                    label="5 - Quantos planetas gasosos existem?"
+                    options={optionsQ5}
+                />
+                {nota}
+                {pontos && '/5'}
+                <button type="submit">Enviar</button>
+            </Form>
         </S.Container>
     );
 }
