099e9874dfcafc176393b28291181f3a80a16a56
caiosev <kaiosev9@gmail.com>
2022-07-17 18:01:36 -0300

    adicionando loading no dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -7,6 +7,7 @@ import Sidebar from '../../components/Sidebar';
 import FilterMobile from '../../components/FilterMobile';
 import axios from '../../services/axios';
 import * as actions from '../../store/modules/auth/actions';
+import Loading from '../../components/Loading';
 
 export default function Dashboard() {
     const [house, setHouse] = useState('hogwarts');
@@ -100,24 +101,23 @@ export default function Dashboard() {
         }
     }, [house, loading === false, nome, allPoints, todosalunos]);
 
+    const loadAlunos = async () => {
+        setLoading(true);
+        const response = await axios.get('/alunos');
+        setTodosAlunos(response.data);
+        const { data } = await axios.get('/casas');
+        setallPoints([
+            ...allPoints,
+            data[0].nota_total,
+            data[1].nota_total,
+            data[2].nota_total,
+            data[3].nota_total,
+        ]);
+        setLoading(false);
+    };
+
     useEffect(() => {
-        async function loadAlunos() {
-            const response = await axios.get('/alunos');
-            setTodosAlunos(response.data);
-            setLoading(false);
-        }
-        async function loadPoints() {
-            const { data } = await axios.get('/casas');
-            setallPoints([
-                ...allPoints,
-                data[0].nota_total,
-                data[1].nota_total,
-                data[2].nota_total,
-                data[3].nota_total,
-            ]);
-        }
         loadAlunos();
-        loadPoints();
     }, []);
 
     const handleLogout = () => {
@@ -147,89 +147,92 @@ export default function Dashboard() {
                 setNome={setNome}
                 nome={nome}
             />
-            <S.Content>
-                <S.Header house={house} color={color}>
-                    <svg
-                        className={`ham ham6 ${menuMobile && 'active'}`}
-                        viewBox="0 0 100 100"
-                        width="50"
-                        onClick={() => setMenuMobile(!menuMobile)}
-                    >
-                        <path
-                            className="line top"
-                            d="m 30,33 h 40 c 13.100415,0 14.380204,31.80258 6.899646,33.421777 -24.612039,5.327373 9.016154,-52.337577 -12.75751,-30.563913 l -28.284272,28.284272"
-                        />
-                        <path
-                            className="line middle"
-                            d="m 70,50 c 0,0 -32.213436,0 -40,0 -7.786564,0 -6.428571,-4.640244 -6.428571,-8.571429 0,-5.895471 6.073743,-11.783399 12.286435,-5.570707 6.212692,6.212692 28.284272,28.284272 28.284272,28.284272"
-                        />
-                        <path
-                            className="line bottom"
-                            d="m 69.575405,67.073826 h -40 c -13.100415,0 -14.380204,-31.80258 -6.899646,-33.421777 24.612039,-5.327373 -9.016154,52.337577 12.75751,30.563913 l 28.284272,-28.284272"
-                        />
-                    </svg>
-                    <Link to="/">
-                        <img src={logo} alt="" />
-                    </Link>
-                    <S.Points points={points} className="points">
-                        <h4>Pontos: {points}</h4>
-                    </S.Points>
-                    <div className="options">
-                        <Link to="/aluno">
-                            <FaPlusCircle size={25} />
+            {!loading && (
+                <S.Content>
+                    <S.Header house={house} color={color}>
+                        <svg
+                            className={`ham ham6 ${menuMobile && 'active'}`}
+                            viewBox="0 0 100 100"
+                            width="50"
+                            onClick={() => setMenuMobile(!menuMobile)}
+                        >
+                            <path
+                                className="line top"
+                                d="m 30,33 h 40 c 13.100415,0 14.380204,31.80258 6.899646,33.421777 -24.612039,5.327373 9.016154,-52.337577 -12.75751,-30.563913 l -28.284272,28.284272"
+                            />
+                            <path
+                                className="line middle"
+                                d="m 70,50 c 0,0 -32.213436,0 -40,0 -7.786564,0 -6.428571,-4.640244 -6.428571,-8.571429 0,-5.895471 6.073743,-11.783399 12.286435,-5.570707 6.212692,6.212692 28.284272,28.284272 28.284272,28.284272"
+                            />
+                            <path
+                                className="line bottom"
+                                d="m 69.575405,67.073826 h -40 c -13.100415,0 -14.380204,-31.80258 -6.899646,-33.421777 24.612039,-5.327373 -9.016154,52.337577 12.75751,30.563913 l 28.284272,-28.284272"
+                            />
+                        </svg>
+                        <Link to="/">
+                            <img src={logo} alt="" />
                         </Link>
-                        <Link to="/usuario">
-                            <FaUserAlt size={25} />
-                        </Link>
-                        <FaSignOutAlt size={25} onClick={handleLogout} />
-                    </div>
-                </S.Header>
-                <FilterMobile
-                    setHouse={setHouse}
-                    house={house}
-                    setNome={setNome}
-                    nome={nome}
-                />
-                <S.ListContainer>
-                    {alunos.map((aluno) => {
-                        return (
-                            <div
-                                className="aluno"
-                                key={String(aluno.id)}
-                                onClick={() => handleClickAluno(aluno.id)}
-                                onKeyDown={() => handleClickAluno(aluno.id)}
-                                role="button"
-                                tabIndex="0"
-                            >
-                                <div className="grid-frame">
-                                    <span className="grid-border" />
-                                    <span className="square tl" />
-                                    <span className="square tr" />
-                                    <span className="square bl" />
-                                    <span className="square br" />
-                                    <img
-                                        className="foto-aluno"
-                                        src={
-                                            aluno['aluno-foto'].length > 0
-                                                ? aluno['aluno-foto'][
-                                                      aluno['aluno-foto']
-                                                          .length - 1
-                                                  ].url
-                                                : 'images/user.png'
-                                        }
-                                        alt=""
-                                    />
+                        <S.Points points={points} className="points">
+                            <h4>Pontos: {points}</h4>
+                        </S.Points>
+                        <div className="options">
+                            <Link to="/aluno">
+                                <FaPlusCircle size={25} />
+                            </Link>
+                            <Link to="/usuario">
+                                <FaUserAlt size={25} />
+                            </Link>
+                            <FaSignOutAlt size={25} onClick={handleLogout} />
+                        </div>
+                    </S.Header>
+                    <FilterMobile
+                        setHouse={setHouse}
+                        house={house}
+                        setNome={setNome}
+                        nome={nome}
+                    />
+                    <S.ListContainer>
+                        {alunos.map((aluno) => {
+                            return (
+                                <div
+                                    className="aluno"
+                                    key={String(aluno.id)}
+                                    onClick={() => handleClickAluno(aluno.id)}
+                                    onKeyDown={() => handleClickAluno(aluno.id)}
+                                    role="button"
+                                    tabIndex="0"
+                                >
+                                    <div className="grid-frame">
+                                        <span className="grid-border" />
+                                        <span className="square tl" />
+                                        <span className="square tr" />
+                                        <span className="square bl" />
+                                        <span className="square br" />
+                                        <img
+                                            className="foto-aluno"
+                                            src={
+                                                aluno['aluno-foto'].length > 0
+                                                    ? aluno['aluno-foto'][
+                                                          aluno['aluno-foto']
+                                                              .length - 1
+                                                      ].url
+                                                    : 'images/user.png'
+                                            }
+                                            alt=""
+                                        />
+                                    </div>
+                                    <h2 className="aluno-nome">
+                                        {' '}
+                                        {`${aluno.nome} ${aluno.sobrenome}`}
+                                    </h2>
+                                    <h3 className="aluno-info">{`${aluno['aluno-casa'].nome} ${aluno['aluno-sala'].nome}`}</h3>
                                 </div>
-                                <h2 className="aluno-nome">
-                                    {' '}
-                                    {`${aluno.nome} ${aluno.sobrenome}`}
-                                </h2>
-                                <h3 className="aluno-info">{`${aluno['aluno-casa'].nome} ${aluno['aluno-sala'].nome}`}</h3>
-                            </div>
-                        );
-                    })}
-                </S.ListContainer>
-            </S.Content>
+                            );
+                        })}
+                    </S.ListContainer>
+                </S.Content>
+            )}
+            {loading && <Loading />}
         </S.Container>
     );
 }
