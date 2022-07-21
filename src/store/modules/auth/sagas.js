c7ae2dbd4a4eeb8dc11d9015d837883fb7577424
caiosev <kaiosev9@gmail.com>
2022-07-21 14:58:32 -0300

    adicionando response no saga

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -8,13 +8,15 @@ import axios from '../../../services/axios';
 function* loginRequest({ payload }) {
     try {
         const response = yield call(axios.post, '/tokens', payload);
+        console.log(response);
         yield put(
             actions.loginSuccess({ ...response.data, options: payload.options })
         );
         axios.defaults.headers.common.Authorization = `Bearer ${response.data.token}`;
         toast.success('Login realizado com sucesso!');
-        if (payload.options === 'prof') payload.navigate('/painel');
-        if (payload.options === 'aluno') payload.navigate('/provas');
+        if (payload.options === 'prof' && response) payload.navigate('/painel');
+        if (payload.options === 'aluno' && response)
+            payload.navigate('/provas');
     } catch (error) {
         console.log(error);
         toast.error('Usuario ou senha incorretos');
