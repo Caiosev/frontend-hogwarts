d536d1248faea06e49a2f3a54159e4930fb4ef93
caiosev <kaiosev9@gmail.com>
2022-07-21 15:07:09 -0300

    yield no axios

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -8,11 +8,10 @@ import axios from '../../../services/axios';
 function* loginRequest({ payload }) {
     try {
         const response = yield call(axios.post, '/tokens', payload);
-        console.log(response);
         yield put(
             actions.loginSuccess({ ...response.data, options: payload.options })
         );
-        axios.defaults.headers.common.Authorization = `Bearer ${response.data.token}`;
+        yield (axios.defaults.headers.common.Authorization = `Bearer ${response.data.token}`);
         toast.success('Login realizado com sucesso!');
         if (payload.options === 'prof' && response) payload.navigate('/painel');
         if (payload.options === 'aluno' && response)
